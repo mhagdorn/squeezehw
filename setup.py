bffcff3896d38546b01b3783e4ff4763d90341a8
Magnus Hagdorn <magnus.hagdorn@marsupium.org>
2018-10-20 09:29:41 +0000

    add setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="squeezehw",
+        version = "0.0",
+        description = "hardware controls for softsqueeze",
+        author = "Magnus Hagdorn",
+        author_email = "magnus.hagdorn@marsupium.org",
+        url = "https://github.com/mhagdorn/squeezehw",
+        scripts = ['controls.py'],
+        )
+
+
